c099d9d8fd2bda7bbfe713ffdda8f912306cc8e2
Kevin Lin <kevin.cj.lin@toastmasters.org.tw>
2023-08-14 22:52:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-document.getElementById('divisionSelect').addEventListener('change', function() {
-    filterResults();
-});
-
-document.getElementById('keywordSearch').addEventListener('input', function() {
+document.getElementById('filterButton').addEventListener('click', function() {
     filterResults();
 });
 
 function filterResults() {
     var selectedDivision = document.getElementById('divisionSelect').value;
+    var selectedCity = document.getElementById('citySelect').value;
+    var selectedMeetingDay = document.getElementById('meetingDaySelect').value;
+    var selectedLanguage = document.getElementById('languageSelect').value;
     var keyword = document.getElementById('keywordSearch').value.toLowerCase();
     var clubs = document.querySelectorAll('#clubList li');
 
     clubs.forEach(function(club) {
         var clubName = club.querySelector('a').textContent.toLowerCase();
-        var clubDivision = club.getAttribute('data-division');
+        var clubFeatures = club.getAttribute('data-features').toLowerCase();
+        var matchesKeyword = clubName.includes(keyword) || clubFeatures.includes(keyword);
 
-        if (clubName.includes(keyword) && clubDivision === selectedDivision) {
+        var clubDivision = club.getAttribute('data-division');
+        var clubCity = club.getAttribute('data-city');
+        var clubMeetingDay = club.getAttribute('data-meeting-day');
+        var clubLanguage = club.getAttribute('data-language');
+
+        if (matchesKeyword && 
+            (clubDivision === selectedDivision || selectedDivision === 'all') &&
+            (clubCity === selectedCity || selectedCity === 'all') &&
+            (clubMeetingDay === selectedMeetingDay || selectedMeetingDay === 'all') &&
+            (clubLanguage === selectedLanguage || selectedLanguage === 'all')) {
             club.style.display = 'block';
         } else {
             club.style.display = 'none';
