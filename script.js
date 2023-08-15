6549c1de218aa4b237a0b517092bbc71a5492371
Kevin Lin <kevin.cj.lin@toastmasters.org.tw>
2023-08-15 11:12:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function filterResults() {
     var selectedDivision = document.getElementById('divisionSelect').value;
     var selectedCity = document.getElementById('citySelect').value;
     var selectedMeetingDay = document.getElementById('meetingDaySelect').value;
+    var selectedClubType = document.getElementById('clubTypeSelect').value;
     var selectedLanguage = document.getElementById('languageSelect').value;
     var keyword = document.getElementById('keywordSearch').value.toLowerCase();
     var clubs = document.querySelectorAll('#clubList li');
@@ -18,12 +19,14 @@ function filterResults() {
         var clubDivision = club.getAttribute('data-division');
         var clubCity = club.getAttribute('data-city');
         var clubMeetingDay = club.getAttribute('data-meeting-day');
+        var clubType = club.getAttribute('data-club-type');
         var clubLanguage = club.getAttribute('data-language');
 
         if (matchesKeyword && 
             (clubDivision === selectedDivision || selectedDivision === 'all') &&
             (clubCity === selectedCity || selectedCity === 'all') &&
             (clubMeetingDay === selectedMeetingDay || selectedMeetingDay === 'all') &&
+            (clubType === selectedClubType || selectedClubType === 'all') &&
             (clubLanguage === selectedLanguage || selectedLanguage === 'all')) {
             club.style.display = 'block';
         } else {
